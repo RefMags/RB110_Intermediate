8e25e0cad77e1e83cefa34e04c69c06b21778f18
Ref Mags <fyfymags@gmail.com>
2024-09-30 06:20:11 +0900

    stringy done

---
+++ lesson_1/pedac_small_problems/easy1_3.rb
@@ -0,0 +1,15 @@
+def stringy(length)
+  arr_numbers = []
+
+  length.times do |index|
+    number = index.even? ? 1 : 0
+    arr_numbers << number
+  end
+  arr_numbers.join
+end
+
+
+puts stringy(6)  == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
