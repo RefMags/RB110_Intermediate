681f2bdbcf9a580cd238ebc838665a4ad17778ce
Ref Mags <fyfymags@gmail.com>
2024-09-29 12:31:15 +0900

    checking odd absolutes done

---
+++ lesson_1/pedac_small_problems/easy1_2.rb
@@ -0,0 +1,11 @@
+def is_odd?(int)
+  abs_value = int.abs
+  abs_value % 2 != 0 ? true : false
+end
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
