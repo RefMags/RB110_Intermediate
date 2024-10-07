1c9d4306866dace080bde1a609004eca3459831a
Ref Mags <fyfymags@gmail.com>
2024-10-08 05:58:19 +0900

    pedac approach solutions

--- exercises/quiz2.rb
+++ exercises/quiz2.rb
@@ -1,8 +1,15 @@
 numbers = ['2', '3', '5', '7', '8', '11', '13', '15', '18']
 odd_numbers = []
+# counter = 0
 
-numbers.each do |number|
-  odd_numbers << number if number.to_i.odd?
+loop do
+  current_number = numbers[counter].to_i
+
+  odd_numbers << current_number unless current_number.even?
+
+  counter += 1
+  break if counter == numbers.size
 end
 
 p odd_numbers
+# => [3, 5, 7, 11, 13, 15]
