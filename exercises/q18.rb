da70d96e6a5e61f9b32c00e6113a2ea6f92142c7
Ref Mags <fyfymags@gmail.com>
2024-10-12 12:39:06 +0900

    partitioning numbers

--- exercises/q18.rb
+++ exercises/q18.rb
@@ -1,6 +1,18 @@
+
 numbers = [1, 2, 4, 5, 7, 8, 10, 13, 14]
-odd_numbers, even_numbers = numbers.partition do |number|
+# Incorrect population
+# odd_numbers, even_numbers = numbers.partition do |number|
+#                               number.even?
+#                             end
+
+
+# p odd_numbers # => [2, 4, 8, 10, 14]
+# p even_numbers # => [1, 5, 7, 13]
+
+# Correct population
+even_numbers, odd_numbers = numbers.partition do |number|
                               number.even?
                             end
+# p odd_numbers # => [2, 4, 8, 10, 14]
+p even_numbers # => [1, 5, 7, 13]
 p odd_numbers
-p even_numbers
