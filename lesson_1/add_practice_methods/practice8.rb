9ac157292ccc65652605a96014ba69abdb16404f
Ref Mags <fyfymags@gmail.com>
2024-10-20 10:46:31 +0900

    understanding effects of destructive methods in 'each' iteration

--- lesson_1/add_practice_methods/practice8.rb
+++ lesson_1/add_practice_methods/practice8.rb
@@ -1,7 +1,15 @@
+# numbers = [1, 2, 3, 4]
+# numbers.each do |number|
+#   p number
+#   numbers.shift(1)
+# end
+
+# numbers
+
 numbers = [1, 2, 3, 4]
-numbers.each do |number|
-  p number
-  numbers.shift(1)
+numbers.each_with_index do |number, index|
+   "#{index} #{numbers.inspect}  #{number}"
+  p numbers.pop(1)
 end
 
-numbers
+p numbers
