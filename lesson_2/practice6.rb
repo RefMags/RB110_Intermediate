84aecbd50079e0332b76ff0e215e1e88d42c1cd5
Ref Mags <fyfymags@gmail.com>
2024-10-30 07:01:11 +0900

    accessing keys and inner key-value pairs of hash_value

---
+++ lesson_2/practice6.rb
@@ -0,0 +1,13 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+# (Name) is a (age)-year-old (male or female).
+
+munsters.each do |names, details|
+  puts "#{names} is a #{details["age"]} #{details["gender"]}."
+end
