321326a3d87f91f9eb9fc11f8f5b4a82a0661691
Ref Mags <fyfymags@gmail.com>
2024-10-30 06:41:02 +0900

    limiting string to three vowels

---
+++ question2.rb
@@ -0,0 +1,46 @@
+def three_vowels(word)
+  word_copy = ''
+  vowels = 'aeiou'
+  vowel_counter = 0
+  counter = 0
+
+  loop do
+    break if counter == word.size
+    current_character = word[counter]
+
+    if !vowels.include?(current_character)
+      word_copy << current_character
+    elsif vowels.include?(current_character)
+        if vowel_counter < 3
+          word_copy << current_character
+          vowel_counter += 1
+        end
+    end
+
+    counter += 1
+  end
+  word_copy
+end
+
+
+# def three_vowels(word)
+#   result = ''
+#   vowels = 'aeiou'
+#   vowel_counter = 0
+
+#   word.each_char do |char|
+#     if vowels.include?(char)
+#       result << char if vowel_counter < 3
+#       vowel_counter += 1 if vowel_counter < 3
+#     else
+#       result << char
+#     end
+#   end
+#   result
+# end
+
+puts three_vowels("bananabanana") == "bananabnn"
+puts three_vowels("tropical") == "tropical"
+puts three_vowels("tropicalia") == "tropical"
+puts three_vowels("BliMp123") == "BliMp123"
+puts three_vowels("eyelash2!!!") == "eyelsh2!!!"
