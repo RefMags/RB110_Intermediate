10508e2f648f78b0df7cd85e1fb31571a237bead
Ref Mags <fyfymags@gmail.com>
2024-11-02 13:38:28 +0900

    performing operation on two digits

--- exercises/prob1.rb
+++ exercises/prob1.rb
@@ -1,42 +1,31 @@
 # # ask for 5 numbers
-# def ask_numbers()
-#   counter = 1
-#   numbers_collection = []
+def ask_numbers()
+  counter = 1
+  numbers_collection = []
 
-#   loop do
-#     break if counter == 6
+  loop do
+    break if counter == 6
 
-#     puts "Enter the #{counter} number:"
-#     user_answer = gets.chomp.to_i
-#     numbers_collection << user_answer
+    puts "Enter the #{counter} number:"
+    user_answer = gets.chomp.to_i
+    numbers_collection << user_answer
 
-#     counter += 1
-#   end
+    counter += 1
+  end
 
-#   numbers_collection
-# end
+  numbers_collection
+end
 
-# five_numbers = ask_numbers()
+five_numbers = ask_numbers()
 # # ask for the 6th number
-# puts "Enter the 6th number"
-# last_num = gets.chomp.to_i
+puts "Enter the 6th number"
+last_num = gets.chomp.to_i
 
 
 # # check if 6th number appears in the array collection
 
-# if five_numbers.include?(last_num)
-#   puts "The number #{last_num} appears in #{five_numbers}"
-# else
-#   puts "The number #{last_num} does not appear in #{five_numbers}"
-# end
-
-numbers = []
-
-%w(1st 2nd 3rd 4th 5th last).each do |indx|
-  puts "Enter the #{indx} number"
-  numbers.push(gets.chomp.to_i)
+if five_numbers.include?(last_num)
+  puts "The number #{last_num} appears in #{five_numbers}"
+else
+  puts "The number #{last_num} does not appear in #{five_numbers}"
 end
-
-last_num = numbers.pop
-appear = numbers.include?(last_num) ? "appear" : "does not appear"
-puts "The number #{last_num} #{present_or_not} in #{numbers}"
---
+++ exercises/prob2.rb
@@ -0,0 +1,25 @@
+# Requesting the numbers
+puts "Enter the 1st number:"
+first_num = gets.chomp.to_i
+puts "Enter the 2nd number:"
+second_num = gets.chomp.to_i
+
+
+# Add
+add_result = first_num + second_num
+puts "Addition: #{add_result}"
+# subtraction
+sub_result = first_num - second_num
+puts "Subration: #{sub_result}"
+# Multiply
+multiple_result = first_num * second_num
+puts "Multiplication: #{multiple_result}"
+# Quotient
+ quotient_result = first_num / second_num
+puts "Quotient: #{quotient_result}"
+# Remainder
+remainder_result = first_num % second_num
+puts "Remainder: #{remainder_result}"
+# Power off
+power_result = first_num ** second_num
+puts "Power: #{power_result}"
