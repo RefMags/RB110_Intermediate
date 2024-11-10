e8e009ecc0a24759b6bb0a0d6516052a05bbd4fb
Ref Mags <fyfymags@gmail.com>
2024-11-10 11:32:48 +0900

    palindromes check

---
+++ exercises/easy3/q7.rb
@@ -0,0 +1,9 @@
+def palindrome?(input)
+  input == input.reverse
+end
+
+
+p palindrome?('madam') == true
+p palindrome?('Madam') == false          # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
+p palindrome?('356653') == true
