c1a0cd6f366844ebfce8f772b0ee020ba622fe78
Ref Mags <fyfymags@gmail.com>
2024-12-10 20:46:23 +0900

    generating pairs manually

--- PEDAC_session/practice6.rb
+++ PEDAC_session/practice6.rb
@@ -49,48 +49,57 @@
 
 =end
 def pairs(numbers)
-  numbers.combination(2).to_a
-end
+  # numbers.combination(2).to_a
+  new_pair = []
 
-def sumOfPairs(pairs_arr)
-  pairs_arr.map { |pair| pair.sum }
-end
-
-def is_perfect_square?(num)
-  return false if num < 0
-
-int = 0
-  while int * int <= num
-    if int * int == num
-      return true
+  numbers.each_with_index do |number , index|
+    (index + 1...numbers.length).each do |idx|
+      new_pair << [number, numbers[idx]]
     end
-    int += 1
-  end
-  false
-end
-
-def sumOfPairsSquares(numbers)
-  all_pairs = pairs(numbers)
-  sums_of_pairs = sumOfPairs(all_pairs)
-
-  perfect_square_pairs = all_pairs.select.with_index do |pair, index|
-    is_perfect_square?(sums_of_pairs[index])
   end
 
-  perfect_square_pairs.sort! {|a, b| a <=> b}
-
-  perfect_square_pairs
+  new_pair
 end
 
-pairs_arr = pairs([1, 2, 3, 4, 5]) == [[1, 2], [1, 3], [1, 4], [1, 5], [2, 3],
-                               [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]
-puts pairs_arr
+# def sumOfPairs(pairs_arr)
+#   pairs_arr.map { |pair| pair.sum }
+# end
 
-puts sumOfPairs([[1, 2], [1, 3], [1, 4], [1, 5], [2, 3],
-                [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]) == [3, 4, 5, 6, 5, 6, 7, 7, 8, 9]
+# def is_perfect_square?(num)
+#   return false if num < 0
 
-puts is_perfect_square?(4) == true
-puts sumOfPairsSquares([1, 2, 3, 4, 5]) == [[1, 3], [4, 5]]
+# int = 0
+#   while int * int <= num
+#     if int * int == num
+#       return true
+#     end
+#     int += 1
+#   end
+#   false
+# end
+
+# def sumOfPairsSquares(numbers)
+#   all_pairs = pairs(numbers)
+#   sums_of_pairs = sumOfPairs(all_pairs)
+
+#   perfect_square_pairs = all_pairs.select.with_index do |pair, index|
+#     is_perfect_square?(sums_of_pairs[index])
+#   end
+
+#   perfect_square_pairs.sort! {|a, b| a <=> b}
+
+#   perfect_square_pairs
+# end
+
+pairs_arr = pairs([1, 2, 3, 4, 5]) #== [[1, 2], [1, 3], [1, 4], [1, 5], [2, 3],
+                               #[2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]
+p pairs_arr
+
+# puts sumOfPairs([[1, 2], [1, 3], [1, 4], [1, 5], [2, 3],
+#                 [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]) == [3, 4, 5, 6, 5, 6, 7, 7, 8, 9]
+
+# puts is_perfect_square?(4) == true
+# puts sumOfPairsSquares([1, 2, 3, 4, 5]) == [[1, 3], [4, 5]]
 
 
 
