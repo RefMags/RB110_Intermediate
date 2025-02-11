f0a7db77e6b42411c5299797ce450b98f6b5c627
Ref Mags <fyfymags@gmail.com>
2025-02-11 19:48:38 +0900

    PEDAC on palindrome problem

---
+++ lesson_1/pedac_small_problems/palindrome.rb
@@ -0,0 +1,18 @@
+=begin
+  ### Problem:
+- We have to create a `palindrome_substrings` method that returns palindrome substrings from a string. We should consider all possible palindromes of size greater than 2. The palindrome words are case sensitive.
+- Explicit: palindrome words are case sensitive
+		- Input an string output an array of substrings
+
+### DS: (Remember include the implementation details for the "hard" parts)
+-
+
+### Algo
+-
+
+=end
+
+# palindrome_substrings("supercalifragilisticexpialidocious") == ["ili"]
+palindrome_substrings("abcddcbA") == ["bcddcb", "cddc", "dd"]
+palindrome_substrings("palindrome") == []
+palindrome_substrings("") == []
