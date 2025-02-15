609a38cf7ca9fdc2cc225a6d2c2d372db7108557
Ref Mags <fyfymags@gmail.com>
2025-02-15 19:34:07 +0900

    add keep score to the game, an infinite loop persisiting

--- lesson_3/7.assignment/keeping_score.rb
+++ lesson_3/7.assignment/keeping_score.rb
@@ -1,28 +1,45 @@
 # Determining the winner
 
 winner = ["Player", "Computer"]
-player_score = 0
-computer_score = 0
+# player_score = 0
+# computer_score = 0
 game_counter = 1
 
-loop do
-  current_winner = winner.sample
+# loop do
+#   current_winner = winner.sample
 
-  if current_winner == "Player"
-    player_score += 1
-    puts "Round #{game_counter}: Player score #{player_score}"
+#   if current_winner == "Player"
+#     player_score += 1
+#     puts "Round #{game_counter}: Player score #{player_score}"
+#   else
+#     computer_score += 1
+#     puts "Round #{game_counter}: Computer score #{computer_score}"
+#   end
+
+#   game_counter += 1
+
+#   break if player_score == 5 || computer_score == 5
+# end
+
+# if player_score == 5
+#   puts "Player reached 5 wins first"
+# else
+#   puts "Computer reached 5 wins first!"
+# end
+
+
+loop do
+  winner_score = { "PLAYER" => 0, "COMPUTER" => 0 }
+  current_winner = winner_score.keys.sample
+  p current_winner
+
+  if current_winner == "PLAYER"
+    winner_score[current_winner] += 1
   else
-    computer_score += 1
-    puts "Round #{game_counter}: Computer score #{computer_score}"
+    winner_score[current_winner] += 1
   end
 
   game_counter += 1
+  break if winner_score["PLAYER"] == 5 || winner_score["COMPUTER"] == 5
 
-  break if player_score == 5 || computer_score == 5
-end
-
-if player_score == 5
-  puts "Player reached 5 wins first"
-else
-  puts "Computer reached 5 wins first!"
 end
