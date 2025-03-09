af10f283392929fd5136ad149e9a475a2fc23a71
Ref Mags <fyfymags@gmail.com>
2025-03-09 17:08:51 +0900

    mapping and retrieving the first three characters done

--- otherSessions/converting_collections.rb
+++ otherSessions/converting_collections.rb
@@ -20,7 +20,32 @@ ages = {"Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn
 
 less_than_100 = ages.select! {|_, value| value < 100}
 
-p less_than_100
-p ages
-
 ## Practice Problem 4____
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+min_age = ages.values.min
+
+## Practice Problem 5____
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.find do |flintstone|
+ if flintstone.start_with?("Be")
+  flintstones.index(flintstone)
+ end
+end
+
+## Practice Problem 6____
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.map! do |flintstone|
+  flintstone.slice(0,3)
+end
+
+flintstones
+
+## Practice Problem 7____
+statement = "The Flintstones Rock"
+p statement_arr = statement.split
+
+# { "F"=>1, "R"=>1, "T"=>1, "c"=>1, "e"=>2, ... }
