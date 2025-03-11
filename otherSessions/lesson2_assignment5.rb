82808ee4bae1b14170393930033b6aead45c7eb8
Ref Mags <fyfymags@gmail.com>
2025-03-11 20:53:11 +0900

    up to no6 done

--- otherSessions/lesson2_assignment5.rb
+++ otherSessions/lesson2_assignment5.rb
@@ -10,4 +10,63 @@ ordered_books = books.sort_by { |book| book[:published] }
 ordered_books1 = books.sort {|a, b| a[:published] <=> b[:published] }
 
 # p ordered_books
-p ordered_books1
+# p ordered_books1
+
+# No4. Finding and replacing all `3`'s with `4`
+def replacement(arr, target, replacement)
+  arr.map do |element|
+    if element.is_a?(Array)
+      replacement(element, target, replacement)
+    elsif element == 3
+      replacement
+    else
+      element
+    end
+  end
+end
+
+arr1 = [1, [2, 3], 4]
+p replacement(arr1, 3, 4)
+
+
+# b
+
+arr2 = [{a: 1}, {b: 2, c: [7, 6, 5], d: 4}, 3]
+# hsh1 = {first: [1, 2, [3]]}
+# hsh2 = {['a'] => {a: ['1', :two, 3], b: 4}, 'b' => 5}
+
+
+# No5. Sum up all the male members's ages
+=begin
+  - we want to access the `hash` values' `"ages"` for key's values.
+  - we sum up all the values
+=end
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_male_age = 0
+
+munsters.each do |_, hash_value|
+  total_male_age += hash_value["age"] if hash_value["gender"] == "male"
+end
+
+# p total_male_age
+
+# No. 6 print out name, age, and gender
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, details|
+  puts "#{name} is a #{details["age"]}-year-old #{details["gender"]}."
+end
