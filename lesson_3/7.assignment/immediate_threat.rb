93ba85964b2f29218a837c8e42c8c57856ca074b
Ref Mags <fyfymags@gmail.com>
2025-03-16 10:39:47 +0900

    refactored some logic into computer defense with some helpers

--- lesson_3/7.assignment/immediate_threat.rb
+++ lesson_3/7.assignment/immediate_threat.rb
@@ -1,21 +1,27 @@
 require 'pry'
-
-def computer_defense(board)
-  # potential loses for the computer == potential winning lines for the player
-  potential_loses = WINNING_LINES.select do |line|
-    board.values_at(*line).count('X') == 2 && board.values_at(*line).include?(" ")
+def player_potential_wins(brd)
+   # potential loses for the computer == potential winning lines for the player
+  potential_player_wins = WINNING_LINES.select do |line|
+    brd.values_at(*line).count('X') == 2 && brd.values_at(*line).include?(" ")
   end
+end
 
+def computer_potential_wins(brd)
+  potential_computer_wins = WINNING_LINES.select do |line|
+    brd.values_at(*line).count('O') == 2 && brd.values_at(*line).include?(" ")
+  end
+end
 
-  if !!potential_loses.empty?
-    potential_win = WINNING_LINES.select do |line|
-      board.values_at(*line).count('O') == 2 && board.values_at(*line).include?(" ")
-    end
-        #  binding.pry
-    secure_win_cell = potential_win[0].find {|cell| board[cell] == ' '}
+def computer_defense(brd)
+  # binding.pry?
+  if !player_potential_wins(brd).empty?
+    player_win = player_potential_wins(brd).sample
+    secure_defense_cell = player_win.find {|cell| brd[cell] == ' '}
+  elsif !computer_potential_wins(brd).empty?
+    computer_win = computer_potential_wins(brd).sample
+    secure_computer_win = computer_win.find {|cell| brd[cell] == ' '}
   else
-    choosen_line = potential_loses.sample
-    empty_cell = choosen_line.find {|cell| board[cell] == ' '}
+    nil
   end
 end
 
@@ -25,7 +31,12 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +   # rows
                 [[1, 5, 9], [3, 5, 7]]                # diagonals
 
 brd1 = {1 => 'X', 2 => ' ', 3 => 'X', 4 => 'O', 5 => 'X', 6 => ' ', 7 => ' ', 8 => ' ', 9 => 'O'}
+# # p player_potential_wins(brd1)
 p computer_defense(brd1)  #== 7 or 2
 
 brd2 = {1 => ' ', 2 => ' ', 3 => 'X', 4 => ' ', 5 => 'X', 6 => ' ', 7 => 'O', 8 => ' ', 9 => 'O'}
+# p computer_potential_wins(brd2)
 p computer_defense(brd2)  #== 8
+
+brd3 = {1 => ' ', 2 => ' ', 3 => 'X', 4 => ' ', 5 => ' ', 6 => ' ', 7 => ' ', 8 => ' ', 9 => ' '}
+p computer_defense(brd3)  #== either 1, 2, 4, 5, 6, 7, 8, 9
