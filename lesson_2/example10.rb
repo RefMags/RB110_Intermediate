7bbd9d2c6373aa4a08beb4ce37323a2d4533bf1b
Ref Mags <fyfymags@gmail.com>
2025-03-17 07:16:21 +0900

    working with complex omultidimensional arrays

--- lesson_2/example10.rb
+++ lesson_2/example10.rb
@@ -1,15 +1,17 @@
-# [[[1, 2], [3, 4]], [5, 6]].map do |arr|
-#   arr.map do |el|
-#     if el.to_s.to_i == el   # it's an integer
-#       el + 1
-#     else                    # it's an array
-#       el.map do |n|
-#         n + 1
-#       end
-#     end
-#   end
-# end
+mapped = [[[1, 2], [3, 4]], [5, 6]].map do |arr|
+  arr.map do |el|
+    if el.to_s.to_i == el   # it's an integer
+      el + 1
+    else                    # it's an array
+      el.map do |n|
+        n + 1
+      end
+    end
+  end
+end
 
+p [1, 2].to_s.to_i
+# p mapped
 # => [[[2, 3], [4, 5]], [6, 7]]
 
 =begin
@@ -25,30 +27,30 @@
 end
 
 =end
-adding_second_element = [5, 6].map do |el|
-  if el.to_s.to_i == el
-    p (el + 1)
-  else
-    el.map do |n|
-     n + 1
-    end
-  end
-end
-p adding_second_element  # => [6, 7]
+# adding_second_element = [5, 6].map do |el|
+#   if el.to_s.to_i == el
+#     p (el + 1)
+#   else
+#     el.map do |n|
+#      n + 1
+#     end
+#   end
+# end
+# p adding_second_element  # => [6, 7]
 
 
-adding_nested_element = [[1, 2], [3, 4]].map do |el| [[]]
-  if el.to_s.to_i == el
-    (el + 1)
-  else
-    el.map do |n| # => [2, 3], [4, 5]
-     p (n + 1)
-    end
-  end
-end
+# adding_nested_element = [[1, 2], [3, 4]].map do |el| [[]]
+#   if el.to_s.to_i == el
+#     (el + 1)
+#   else
+#     el.map do |n| # => [2, 3], [4, 5]
+#      p (n + 1)
+#     end
+#   end
+# end
 
-p adding_nested_element # => [[2, 3], [4, 5]]
+# p adding_nested_element # => [[2, 3], [4, 5]]
 
-[1, 2].map do |n| # => [2, 3]
-     p (n + 1)
-    end
+# [1, 2].map do |n| # => [2, 3]
+#      p (n + 1)
+#     end
