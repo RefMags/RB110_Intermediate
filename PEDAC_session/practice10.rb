781eb07539437b9f367b8772ffc4ea2181d0d533
Ref Mags <fyfymags@gmail.com>
2025-03-19 05:23:26 +0900

    working on  summing up integers from a string

---
+++ PEDAC_session/practice10.rb
@@ -0,0 +1,16 @@
+
+
+def sum_of_numbers(str)
+  result = []
+  pairs = ""
+
+  str.each_char do |char|
+
+  end
+end
+
+sum_of_numbers("HE2LL3O") == 23
+# sum_of_numbers("HE20LL3O") == 203
+# p sum_of_numbers("L12aun3ch Sch3oo451") #= 469
+# p sum_of_numbers("HE2LL3O W1OR5LD") == 11
+# p sum_of_numbers("The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog") == 3635
