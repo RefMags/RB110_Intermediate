11cbdfe6d615fc8a183f44fc22429d080375c5fe
Ref Mags <fyfymags@gmail.com>
2025-04-26 11:55:30 +0900

    counting the number of even  substrings

---
+++ prep/p10_even_substrings.rb
@@ -0,0 +1,17 @@
+def even_substrings(str)
+  total = 0
+
+  str.each_char.with_index do |char, idx|
+    (idx..str.size - 1 ).each do |idx2|
+      total += 1 if str[idx..idx2].to_i % 2 == 0
+    end
+  end
+
+  total
+end
+
+p even_substrings('1432') == 6
+p even_substrings('3145926') == 16
+p even_substrings('2718281') == 16
+p even_substrings('13579') == 0
+p even_substrings('143232') == 12
