f5d73b7a8e5c72b49753eab3e7cffaa73f08f50d
Ref Mags <fyfymags@gmail.com>
2025-06-08 11:05:57 +0900

    multiples of 7 and 11 to the target

--- prep/p14_seven_eleven.rb
+++ prep/p14_seven_eleven.rb
@@ -28,7 +28,7 @@ def seven_eleven(int)
   multiples = []
 
   # find all multiples of 7
-  (0.upto(int - 1)).each do |num|
+  (1.upto(int - 1)).each do |num|
     if num % 7 == 0 || num % 11 == 0
       multiples << num
     end
@@ -45,3 +45,43 @@ p seven_eleven(25) == 75
 p seven_eleven(100) == 1153
 p seven_eleven(0) == 0
 p seven_eleven(-100) == 0
+
+
+=begin
+  Second aApproach
+def multiples_of_seven(target)
+  seven_multiples = []
+
+  (1.upto(target)).each do |num|
+    product = num * 7
+    seven_multiples << product if product < target
+  end
+
+  seven_multiples
+end
+
+def multiples_of_eleven(target)
+  eleven_multiples = []
+
+  (1.upto(target)).each do |num|
+    product = num * 11
+    eleven_multiples << product if product < target
+  end
+  eleven_multiples
+end
+
+def seven_eleven(target)
+  multiples = multiples_of_seven(target)+ multiples_of_eleven(target)
+
+  sum = multiples.uniq.sum
+end
+
+p seven_eleven(10) == 7
+p seven_eleven(11) == 7
+p seven_eleven(12) == 18
+p seven_eleven(25) == 75
+p seven_eleven(100) == 1153
+p seven_eleven(0) == 0
+p seven_eleven(-100) == 0
+
+=end
