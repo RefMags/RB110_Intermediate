53171ebe607cc3305c6515c851d978eece2973b6
Ref Mags <fyfymags@gmail.com>
2025-06-09 20:42:11 +0900

    p15 greatest product #

--- prep/p15_greatest_product.rb
+++ prep/p15_greatest_product.rb
@@ -5,10 +5,6 @@ def greatest_product(str)
   nums = str.to_i.digits.reverse
   subs = []
 
-  # nums.each do |num|
-
-  # end
-  #
   (0..nums.size - 4).each do |num|
     subs << nums[num, 4]
   end
@@ -20,3 +16,27 @@ p greatest_product('23456') == 360      # 3 * 4 * 5 * 6
 p greatest_product('3145926') == 540    # 5 * 9 * 2 * 6
 p greatest_product('1828172') == 128    # 1 * 8 * 2 * 8
 p greatest_product('123987654') == 3024 # 9 * 8 * 7 * 6
+
+
+=begin
+  Approach 2
+
+def greatest_product(source)
+  result = 0
+  numbers = numbers.to_i.digits.reverse
+
+  (0..(source.size - 4)).each do |idx|
+    numbers = numbers[idx, 4]
+
+    product = numbers.inject(:*)
+    result = product if product > result
+  end
+
+  result
+end
+p greatest_product('23456') == 360      # 3 * 4 * 5 * 6
+p greatest_product('3145926') == 540    # 5 * 9 * 2 * 6
+p greatest_product('1828172') == 128    # 1 * 8 * 2 * 8
+p greatest_product('123987654') == 3024 # 9 * 8 * 7 * 6
+
+=end
