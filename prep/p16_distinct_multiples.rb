1bc83af5b03373866b6a76be242e641c9e71709e
Ref Mags <fyfymags@gmail.com>
2025-06-09 20:48:12 +0900

    distinct multiples

--- prep/p16_distinct_multiples.rb
+++ prep/p16_distinct_multiples.rb
@@ -19,3 +19,24 @@ p distinct_multiples('multiplicity') == 3      # l, t, i
 p distinct_multiples('7657') == 1              # 7
 p distinct_multiples('3141592653589793') == 4  # 3, 1, 5, 9
 p distinct_multiples('2718281828459045') == 5  # 2, 1, 8, 4, 5
+
+
+=begin
+def distinct_multiples(source)
+  source = source.downcase.chars
+
+  chars_frequency = source.tally
+
+  chars_frequency.select {|k, v| v > 1}.count
+
+end
+
+p distinct_multiples('xyz') == 0               # (none
+p distinct_multiples('xxyypzzr') == 3          # x, y, z
+p distinct_multiples('xXyYpzZr') == 3          # x, y, z
+p distinct_multiples('unununium') == 2         # u, n
+p distinct_multiples('multiplicity') == 3      # l, t, i
+p distinct_multiples('7657') == 1              # 7
+p distinct_multiples('3141592653589793') == 4  # 3, 1, 5, 9
+p distinct_multiples('2718281828459045') == 5  # 2, 1, 8, 4, 5
+=end
